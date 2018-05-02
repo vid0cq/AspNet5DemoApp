f74c833613451f7ef8121f7a4978254e088e4598
CHIRA  Victor <VChira@iss.local>
2018-05-02 11:08:14 +0300

    updatestatus endpoint

--- src/AspNetCoreDemoApp/Controllers/ValuesController.cs
+++ src/AspNetCoreDemoApp/Controllers/ValuesController.cs
@@ -13,15 +13,22 @@ namespace AspNetCoreDemoApp.Controllers
 			return new[] { "The status of your tax return is awaiting for client response" };
 		}
                 
-                // POST: api/values
-                [HttpPost]
-                public object Post()
+        // POST: api/values
+        [HttpPost]
+        public object Post()
 		{
-                        return new { version = "1.0", response = new { outputSpeech = new { type = "PlainText", text = "The status of your tax return is awaiting for client response" } } };
+            return new { version = "1.0", response = new { outputSpeech = new { type = "PlainText", text = "The status of your tax return is awaiting for client response" } } };
 		}
-                
-		// GET api/values/5
-		[HttpGet("{id}")]
+
+        [Route("updateStatus")]
+        [HttpPost]
+        public object UpdateState(string taxyear, string UTR, int status)
+        {
+            return new { response = "Status updated to " + status + " for " + UTR + " for taxyear " + taxyear };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
 		public string Get(int id)
 		{
 			return "value" + id + "change(s)";
